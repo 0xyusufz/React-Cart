24fe60cd38a943260cc8a5b826b2f194bfac5f65
0xyusufz <gurucoc471@gmail.com>
2026-03-02 15:45:02 +0530

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-
 import React from "react";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Toaster} from "react-hot-toast"
 import Home from './components/Home'
 import Header from './components/Header'
 
 import "./styles/app/app.scss";
 import "./styles/colors/colors.scss"
 import "./styles/header/header.scss"
+import "./styles/home/home.scss"
+import Cart from "./components/Cart";
 
 const App = () => {
   return (
@@ -16,7 +16,9 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+      <Toaster />
     </BrowserRouter>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import "../styles/home/home.scss";
+import Mackbook from "../assets/img1.avif";
+import iphone from "../assets/img2.jpeg";
+import toast from "react-hot-toast";
 
 const Home = () => {
+  let productList = [
+    {
+      name: "Mac Book",
+      price: 89000,
+      imgSrc: Mackbook,
+      id: "p1",
+    },
+    {
+      name: "iPhone 15",
+      price: 75000,
+      imgSrc: iphone,
+      id: "p2",
+    },
+  ];
+  const addToCartHandler = (options) => {
+    console.log(options);
+    toast.success("Added to cart");
+  };
   return (
-    <div>
-      <h1>home</h1>
+    <div className="home">
+      {productList.map(function (elem) {
+        return (
+          <ProductCard
+            key={elem.id}
+            name={elem.name}
+            id={elem.id}
+            price={elem.price}
+            imgSrc={elem.imgSrc}
+            handler={addToCartHandler}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+const ProductCard = ({ name, id, price, handler, imgSrc }) => {
+  return (
+    <div className="productCard">
+      <img src={imgSrc} alt={name} />
+      <p>{name}</p>
+      <h4>${price}</h4>
+      <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>
+        add to cart
+      </button>
+    </div>
+  );
+};
+
+export default Home;
